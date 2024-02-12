92a5ea47b9ce7ed4a18fa88a6b078f1ffd831d37
Nikolay <sarody@ya.ru>
2024-02-12 13:58:22 +0300

    change BrowserRouter to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './app/App'
 import './app/styles/index.scss'
 import { ErrorBoundary } from './app/providers/ErrorBoundary'
@@ -7,11 +7,11 @@ import { StoreProvider } from './app/providers/StoreProvider'
 
 createRoot(document.getElementById('root')!)
     .render(
-        <BrowserRouter>
+        <HashRouter>
             <StoreProvider>
                 <ErrorBoundary>
                     <App />
                 </ErrorBoundary>
             </StoreProvider>
-        </BrowserRouter>,
+        </HashRouter>,
     )
